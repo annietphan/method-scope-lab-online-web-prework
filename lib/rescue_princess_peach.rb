2aaf14d8da0de80c500802d91e505864e8ab0166
Annie Phan <anniephan.atp@gmail.com>
2019-02-26 19:57:09 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,7 +1,12 @@
-rescue_princess_peach
-puts status
-
 def rescue_princess_peach
   status = "rescued"
   puts "Hooray! Mario has rescued Princess Peach."
 end
+
+
+
+
+
+
+rescue_princess_peach
+puts status
